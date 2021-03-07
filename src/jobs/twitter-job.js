62292116344dfb01683ed9b707b6f7b8d63c4aab
EduardoNatale <eduardon.work@gmail.com>
2021-03-07 12:05:37 -0300

    Job twitter

--- src/jobs/twitter-job.js
+++ src/jobs/twitter-job.js
@@ -2,7 +2,12 @@ const { CronJob } = require('cron');
 const twitterService = require('../services/twitter-service');
 
 // segundos minutos horas
-(new CronJob('00 00 12 * * *', (() => {
+(new CronJob('00 00 06 * * *', (() => {
+  const channel = global.discordClient.channels.cache.find((c) => c.name.includes('twitter'));
+  twitterService.timeline(channel);
+}))).start();
+
+(new CronJob('00 00 14 * * *', (() => {
   const channel = global.discordClient.channels.cache.find((c) => c.name.includes('twitter'));
   twitterService.timeline(channel);
 }))).start();
