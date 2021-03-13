da2ce49576e9bae2e4d17e705ebdb40a8d3d9f6d
EduardoNatale <eduardon.work@gmail.com>
2021-03-13 14:15:48 -0300

    Message at 6 pm

--- src/jobs/twitter-job.js
+++ src/jobs/twitter-job.js
@@ -11,5 +11,5 @@ const twitterService = require('../services/twitter-service');
 // Will send message at 6 pm
 (new CronJob('00 00 21 * * *', (() => {
   const channel = global.discordClient.channels.cache.find((c) => c.name.includes('league-of-legends'));
-  twitterService.timeline(channel, '');
+  twitterService.timeline(channel, 'LoLegendsBR');
 }))).start();
